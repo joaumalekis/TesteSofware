683a39a53ab7e846e4d16b9b9f1171f34b41b043
joaoalexrodrigues <joaum@outlook.com>
2021-08-23 22:06:11 -0300

    TDD

---
+++ 01 - Testes de Unidade/Feature.Tests/Features.Tests/03 - Order/OrdemTestes.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace Features.Tests
+{
+    [TestCaseOrderer("Features.Tests.PriorityOrderer", "Features.Tests")]
+    public class OrdemTestes
+    {
+        private static bool _teste1Chamado;
+        private static bool _teste2Chamado;
+        private static bool _teste3Chamado;
+        private static bool _teste4Chamado;
+
+        [Fact(DisplayName = "Teste 04"), TestPriority(3)]
+        [Trait("Categoria", "Ordenacao Testes")]
+        public void Teste04()
+        {
+            _teste4Chamado = true;
+
+            Assert.True(_teste3Chamado);
+            Assert.True(_teste1Chamado);
+            Assert.False(_teste2Chamado);
+        }
+
+        [Fact(DisplayName = "Teste 01"), TestPriority(2)]
+        [Trait("Categoria", "Ordenacao Testes")]
+        public void Teste01()
+        {
+            _teste1Chamado = true;
+
+            Assert.True(_teste3Chamado);
+            Assert.False(_teste4Chamado);
+            Assert.False(_teste2Chamado);
+        }
+
+        [Fact(DisplayName = "Teste 03"), TestPriority(1)]
+        [Trait("Categoria", "Ordenacao Testes")]
+        public void Teste03()
+        {
+            _teste3Chamado = true;
+
+            Assert.False(_teste1Chamado);
+            Assert.False(_teste2Chamado);
+            Assert.False(_teste4Chamado);
+        }
+
+        [Fact(DisplayName = "Teste 02"), TestPriority(4)]
+        [Trait("Categoria", "Ordenacao Testes")]
+        public void Teste02()
+        {
+            _teste2Chamado = true;
+
+            Assert.True(_teste3Chamado);
+            Assert.True(_teste4Chamado);
+            Assert.True(_teste1Chamado);
+        }
+    }
+}
---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar Item Novo Pedido Vazio")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
+            
+            // Act
+            pedido.AdicionarItem(pedidoItem);
+            
+            // Assert
+            Assert.Equal(200, pedido.ValorTotal);
+        }
+
+        [Fact(DisplayName = "Adicionar Item Novo Pedido Existente")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_ItemExistente_DeveIncrementarUnidadesSomarValores()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var produtoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(produtoId, "Produto Teste", 2, 100);
+            pedido.AdicionarItem(pedidoItem);
+            var pedidoItem2 = new PedidoItem(produtoId, "Produto Teste", 1, 100);
+            
+            // Act
+            pedido.AdicionarItem(pedidoItem2);
+            
+            // Assert 
+            Assert.Equal(300, pedido.ValorTotal);
+            Assert.Equal(1, pedido.PedidoItems.Count);
+            Assert.Equal(3, pedido.PedidoItems.FirstOrDefault(p=>p.ProdutoId == produtoId).Quantidade);
+        }
+    }
+}
